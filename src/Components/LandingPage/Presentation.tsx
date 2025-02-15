41830cc2b718b984a7db12fc79779239b770830e
HidDen-MaChinA <hei.franco.3@gmail.com>
2025-02-15 17:12:29 +0100

    feat: Presentation component structure setted up

---
+++ src/Components/LandingPage/Presentation.tsx
@@ -0,0 +1,13 @@
+export default function(){
+    return(
+        <div className="h-[80vh] p-4 w-full bg-black">
+            <div className="justify-between items-center h-full flex">
+                <div></div>
+                <div className="flex gap-5 text-3xl text-white flex-col">
+                    <p>Hello I'm Franco, a software engeneer.</p>
+                    <p>Pationate by UI creation.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
