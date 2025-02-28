d3b16df7dff56753239fdb3e8101225f6f85ebc6
HidDen-MaChinA <hei.franco.3@gmail.com>
2025-02-28 08:30:24 +0100

    feat: Friend component

---
+++ src/Components/Messages/Friend.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react"
+
+export type FriendProps = {
+    expanded: boolean
+    name: string
+    lastMessage?: string
+    profilePicture?: string
+    online?: boolean
+}
+
+
+export default function Friend(props: FriendProps) { 
+    const { expanded, name, lastMessage, online } = props;
+    const [style, setStyle] = useState<React.CSSProperties>({});
+    useEffect(()=>{
+        if(expanded){
+            setTimeout(()=>{
+                setStyle(_=>({..._,
+                    transitionDuration: "400ms",
+                    position: "relative",
+                    zIndex: 1,
+                    opacity: 1
+                }))
+            }, 200)
+        }else{
+            setStyle(_=>({..._,
+                transitionDuration: "0ms",
+                zIndex: -1,
+                opacity: 0
+            }))
+        }
+    }, [expanded])
+    return (
+        <div className="w-full py-2 flex items-center relative rounded-xl hover:brightness-150 transition-[500ms] hover:bg-[#1f1f1f]">
+            <div className="rounded-full w-[40px] absolute left-[5px] block h-[40px] bg-white">
+                <img src="" alt="" className="w-full h-full rounded-full" />
+                <div className="bg-black right-0 bottom-[-5px] p-1 rounded-full absolute">
+                    <div className="rounded-full h-[10px] w-[10px]" style={{backgroundColor: online ? "green" : "red"}}></div>
+                </div>
+            </div>
+            <div className="text-white px-2 flex-1 relative ml-[50px]" style={style}>
+                <h4 className="bold">{name}</h4>
+                <p className="text-gray-500 w-[200px] text-sm text-ellipsis whitespace-nowrap overflow-hidden">{lastMessage || name + " was added !"}</p>
+            </div>
+        </div>
+    )
+}
--- src/Pages/Messages.tsx
+++ src/Pages/Messages.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import Friend from "../Components/Messages/Friend";
 import Sidebar from "../Components/Messages/Sidebar";
 
 export default function Messages(){
+    const [isOpened, setIsOpened] = useState(false);
+    const arr = []
+    for (let i = 0; i < 15; i++) {
+        arr.push(i);
+    }
     return (
         <div className="w-[100vw] h-[100vh]">
-            <Sidebar closedWidth={50} openedWidth={200} />
+            <Sidebar closedWidth={60} sidebarStateModifier={setIsOpened} openedWidth={300} title="Messages">
+                <div className="p-[5px] mt-[5px] flex overflow-x-hidden flex-col h-full" style={{overflowY: isOpened ? "auto" : "hidden"}}>
+                    {arr.map((i)=>(
+                        <Friend expanded={isOpened} online={i % 2 == 0} lastMessage="hellow boy how you doing ? last night was crazy huh ?" name={"COCO" + i}/>
+                    ))}
+                </div>
+            </Sidebar>
         </div>
     )
 }
