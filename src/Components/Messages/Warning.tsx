2917ee2673904ba2a808f46b624a71643cd5f5c9
HidDen-MaChinA <hei.franco.3@gmail.com>
2025-03-02 07:57:45 +0100

    feat: Warning component used for warning users

---
+++ src/Components/Messages/Warning.tsx
@@ -0,0 +1,33 @@
+import Button from "../Login/Button";
+import Modal from "../Modal";
+
+
+type WarningProps = {
+  message?: string
+  accept?: ()=>void
+  reject?: ()=>void
+}
+
+export default function Warning(props: WarningProps){
+  const {accept, message, reject} = props
+    return (
+      <Modal
+        button={<div className="h-[35px] w-[35px] bg-black rounded-full"></div>}
+      >
+        <div className="flex flex-col">
+          <div>
+            {message}
+          </div>
+          <hr className="w-[90%]"/>
+          <div className="w-full gap-3 flex justify-end">
+            <Button width="max-content" effectColor="green">
+              YES !
+            </Button>
+            <Button width="max-content" effectColor="red">
+              NO !!!
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    );
+}
