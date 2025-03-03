f3e521ce11bd4cf10921385edecd0e571ee91f62
HidDen-MaChinA <hei.franco.3@gmail.com>
2025-03-03 08:52:29 +0100

    feat: Register component

---
+++ src/Pages/Register.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import Button from "../Components/Login/Button";
+
+export default function Register() {
+  const [email, setEmail] = useState({ value: "", ok: true });
+  const [password, setPassword] = useState({ value: "", ok: true });
+  const expression = /<|>/g;
+  const emailChangeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (inputIsValid(e.target.value, expression)) {
+      setEmail({ value: e.target.value, ok: true });
+    } else {
+      setEmail({ value: e.target.value, ok: false });
+    }
+  };
+
+  const passwordChangeEventHandler = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (inputIsValid(e.target.value, expression)) {
+      setPassword({ value: e.target.value, ok: true });
+    } else {
+      setPassword({ value: e.target.value, ok: false });
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex flex-row h-[100vh] w-[100vw]">
+        <div className="w-1/2 flex justify-center items-center">
+          <div className="items-center flex flex-col">
+            <div className="w-[100px] h-[100px] relative">
+              <img src="/images/icons/source-code.svg" alt="" className="w-full h-full" />
+            </div>
+            <h1 className="w-full text-center text-xl">Dark Thunder</h1>
+          </div>
+        </div>
+        <div className="w-1/2 flex bg-black justify-center items-center">
+          <div className="px-5 bg-white shadow-lg rounded-xl flex-col shadow-gray-600 flex gap-2">
+            <h1 className="text-2xl w-full text-center py-4">Login</h1>
+            <p>Email</p>
+            <input
+              type="text"
+              className="rounded-md p-3 py-2 w-full border"
+              onChange={emailChangeEventHandler}
+              style={{ border: email.ok ? "" : "1px solid red" }}
+            />
+            <Button>
+              Register
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function inputIsValid(arg: string, expression: RegExp): Boolean {
+  return arg.match(expression) ? false : true;
+}
