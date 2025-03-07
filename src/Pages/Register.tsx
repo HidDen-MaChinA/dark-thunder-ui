4568e084b593f0b352f260a0bf7e7898334bd115
HidDen-MaChinA <hei.franco.3@gmail.com>
2025-03-07 18:43:44 +0100

    fix: change button text, add note after input

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -3,23 +3,12 @@ import Button from "../Components/Login/Button";
 
 export default function Register() {
   const [email, setEmail] = useState({ value: "", ok: true });
-  const [password, setPassword] = useState({ value: "", ok: true });
   const expression = /<|>/g;
   const emailChangeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (inputIsValid(e.target.value, expression)) {
       setEmail({ value: e.target.value, ok: true });
     } else {
       setEmail({ value: e.target.value, ok: false });
-    }
-  };
-
-  const passwordChangeEventHandler = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    if (inputIsValid(e.target.value, expression)) {
-      setPassword({ value: e.target.value, ok: true });
-    } else {
-      setPassword({ value: e.target.value, ok: false });
     }
   };
 
@@ -38,15 +27,18 @@ export default function Register() {
           <div className="px-5 bg-white shadow-lg rounded-xl flex-col shadow-gray-600 flex gap-2">
             <h1 className="text-2xl w-full text-center py-4">Register</h1>
             <p>Email</p>
-            <input
-              type="text"
-              className="rounded-md p-3 py-2 w-full border"
-              onChange={emailChangeEventHandler}
-              style={{ border: email.ok ? "" : "1px solid red" }}
-            />
+            <div>
+              <input
+                type="text"
+                className="rounded-md p-3 py-2 w-full border"
+                onChange={emailChangeEventHandler}
+                style={{ border: email.ok ? "" : "1px solid red" }}
+              />
+              <p className="text-blue-500 ">* we need to verify your email first</p>
+            </div>
             <div className="p-3">
               <Button >
-                Register
+                Verify
               </Button>
             </div>
           </div>
