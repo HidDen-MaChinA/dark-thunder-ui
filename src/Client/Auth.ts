6ec6c4aef1dbb855598b8e4b4cd62feb2f9e6168
HidDen-MaChinA <hei.franco.3@gmail.com>
2025-03-08 08:19:22 +0100

    fix: code style

--- src/Client/Auth.ts
+++ src/Client/Auth.ts
@@ -1,29 +1,31 @@
-import { UserAuthentified } from "../@types/User"
-import { axiosClient } from "../axiosClient"
+import { UserAuthentified } from "../@types/User";
+import { axiosClient } from "../axiosClient";
 
 export type AuthClientType = {
-    logout: () => Promise<boolean>
-    login: (arg: LoginUser) => Promise<UserAuthentified>
-    whoami: () => Promise<UserAuthentified>
-}
+  logout: () => Promise<boolean>;
+  login: (arg: LoginUser) => Promise<UserAuthentified>;
+  whoami: (userSession: string) => Promise<UserAuthentified>;
+};
 
 export type LoginUser = {
-    email: string
-    password: string
-}
+  email: string;
+  password: string;
+};
 
-export const auth:AuthClientType = {
-    logout: async ()=>{
-        return axiosClient.post("/logout").then(()=>{
-            return true
-        }) 
-       },
-    login: async (arg: LoginUser)=>{
-        return axiosClient.post<UserAuthentified>("/logout", arg).then((res)=>{
-            return res.data;
-        })    },
-    whoami: async ()=>{
-        return axiosClient.post<UserAuthentified>("/logout").then((res)=>{
-            return res.data;
-        })    }
-} 
+export const authClient: AuthClientType = {
+  logout: async () => {
+    return axiosClient.post("/logout").then(() => {
+      return true;
+    });
+  },
+  login: async (arg: LoginUser) => {
+    return axiosClient.post<UserAuthentified>("/logout", arg).then((res) => {
+      return res.data;
+    });
+  },
+  whoami: async (userSession) => {
+    return axiosClient.get<UserAuthentified>("/logout", {headers: {Authorization: userSession}}).then((res) => {
+      return res.data;
+    });
+  },
+};
