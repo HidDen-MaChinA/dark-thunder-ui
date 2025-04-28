b62d1f12cbf9bf13d3380c91ee081ebbe9fc10a6
HidDen-MaChinA <hei.franco.3@gmail.com>
2025-04-28 18:18:57 +0200

    feat: more ressources providers for /api/user/friendship

--- src/Providers/UserFriendshipProvider.ts
+++ src/Providers/UserFriendshipProvider.ts
@@ -1,13 +1,42 @@
+import { Friendship } from "../@types/Friendships"
 import { AxiosClient } from "../AxiosClient"
 
 type UserFriendshipProviderType = {
-    create: (user_id:string)=> any
+    create: (args:{user_id:string})=>Promise<Friendship>
+    delete: (args:{friendship_id: string})=>Promise<number>
+    allow: (args:{friendship_id:string})=>Promise<number>
+    getAllReceived: (args:{page: number})=>Promise<Friendship[]>
+    getAllSent: (args:{page: number})=>Promise<Friendship[]>
 }
 
 export const UserFriendshipProvider : UserFriendshipProviderType = {
-    create: (user_id)=>{
-        AxiosClient.post("/api/user/friendship/create", {
-            "user_id" : user_id
+    create: (args)=>{
+        return AxiosClient.post<Friendship>("/api/user/friendship/create", args).then((res)=>{
+            return res.data
+        })
+    },
+
+    delete: (args)=>{
+        return AxiosClient.post<number>("/api/user/friendship/delete", args).then((res)=>{
+            return res.data
+        })
+    },
+
+    allow: (args)=>{
+        return AxiosClient.post<number>("/api/user/friendship/delete", args).then((res)=>{
+            return res.data
+        })
+    },
+
+    getAllReceived: (args)=>{
+        return AxiosClient.get<Friendship[]>(`/api/user/friendships/received?page=${args.page}`).then((res)=>{
+            return res.data
+        })
+    },
+
+    getAllSent: (args)=>{
+        return AxiosClient.get<Friendship[]>(`/api/user/friendships/received?page=${args.page}`).then((res)=>{
+            return res.data
         })
     }
 }
